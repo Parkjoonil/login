9d908334fe455c14e479aa8c4c11f489d8f0aa03
Parkjoonil <106866130+Parkjoonil@users.noreply.github.com>
2022-07-27 02:11:41 +0900

    login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,62 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'main',
+    component: MainComponent
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
+]
+
+// const routes: Routes = [
+//   {
+//     path: '',
+//     component: LoginComponent,
+//     children: [
+//       {
+//         path: '',
+//         redirectTo: 'login',
+//         pathMatch: 'full',
+//       },
+//       {
+//         path: 'login',
+//         loadChildren: () => import('./login/login.component').then(m => m.LoginComponent),
+//       },
+//       {
+//         path: 'signup',
+//         loadChildren: () => import('./signup/signup.component').then(m => m.SignupComponent),
+//       },
+//     ]
+    
+//   },
+//   {
+//     path: '',
+//     canActivate: [AuthGuard],
+//     children: [
+//       {
+//         path: 'main',
+//         loadChildren: () => import('./main/main.component').then(m => m.MainComponent),
+//       }
+//     ]
+//   }
+// ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
